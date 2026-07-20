55451756085f1bac1ea353e211d56f44d27a4c18
sepanta <spnta40@gmail.com>
2026-07-21 01:48:55 +0330

    Add 32-bit Xray support + auto architecture detection

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Run Xray with config.json
-./xray/xray -config config.json
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "aarch64" ]]; then
+    echo "Detected 64-bit device"
+    ./xray/xray -config config.json
+else
+    echo "Detected 32-bit device"
+    ./xray32/xray -config config.json
+fi
